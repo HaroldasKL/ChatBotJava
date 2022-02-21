b1cc6378fe8e391884eb672e51570e88f5315c8d
HaroldasKL <haroldas.klangauskas@gmail.com>
2022-02-21 15:36:06 +0200

    guessinGame added

--- src/main.java
+++ src/main.java
@@ -1,8 +1,85 @@
 import java.util.Scanner;
 import java.time.*;
+import java.util.Random;
+
 public class main {
-    public static boolean onlyLetters(String name) {
+    public static boolean onlyLetters(String name)
+    {
         return name.matches("[a-zA-Z]+");
+    }
+
+    public static void guessingGame()
+    {
+        int guess = -1;
+        int computerGuess = -1;
+
+        boolean didIGuess = false;
+
+        int startNumber = 0;
+        int endNumber = 100;
+
+        Random rand = new Random();
+        int random = rand.nextInt(101);
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("ATSAKYMAS: " + random);
+        while(guess != random || computerGuess != random)
+        {
+            if(!didIGuess)
+            {
+                System.out.println("Your guess from the interval [" + startNumber + "," + endNumber + "]?");
+                guess = myObj.nextInt();
+
+                if(guess >= startNumber && guess <= endNumber)
+                {
+                    didIGuess = true;
+                    if(guess > random)
+                    {
+                        System.out.println("Number is lower");
+                        System.out.println("-----------------");
+                        endNumber = guess - 1;
+                    }
+                    else if(guess < random)
+                    {
+                        System.out.println("Number is higher");
+                        System.out.println("-----------------");
+                        startNumber = guess + 1;
+                    }
+                    else
+                    {
+                        System.out.println("You won! But this game is based on luck... \n");
+                        break;
+                    }
+                }
+                else
+                {
+                    System.out.println("Enter a number within the interval");
+                }
+            }
+            else
+            {
+                didIGuess = false;
+                computerGuess = startNumber + rand.nextInt((endNumber - startNumber + 1));
+                System.out.println("Charlie's guess :(from the interval)[" + startNumber + "," + endNumber + "]: " + computerGuess);
+                if(computerGuess > random)
+                {
+                    System.out.println("Number is lower");
+                    System.out.println("-----------------");
+                    endNumber = computerGuess - 1;
+                }
+                else if(computerGuess < random)
+                {
+                    System.out.println("Number is higher");
+                    System.out.println("-----------------");
+                    startNumber = computerGuess + 1;
+                }
+                else
+                {
+                    System.out.println("I won! You'r a loser \n");
+                    break;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -177,6 +254,28 @@ public class main {
         else
         {
             System.out.println("Good evening " + name + surname);
+        }
+
+        while(op != 0)
+        {
+            System.out.println("What would you like to do? \n" +
+                            "1 - Play a number guessing game against me \n" +
+                            "2 - Find the next number in a sequence \n" +
+                            "0 - Exit \n");
+            op = myObj.nextInt();
+
+            if(op == 1)
+            {
+                guessingGame();
+            }
+            else if(op == 2)
+            {
+//                randomSequence();
+            }
+            else if(op == 0)
+            {
+                System.out.println("Goodbye!");
+            }
         }
     }
 }
